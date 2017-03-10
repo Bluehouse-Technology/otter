6e095093c4454d87872fc4150be5fdad95c89e84
fholzhauser <ferenc.holzhauser@gmail.com>
2017-03-10 13:31:38 +0100

    Add HTTP/Thrift (Zipkin protocol) server

--- src/otter_sup.erl
+++ src/otter_sup.erl
@@ -11,4 +11,31 @@ start_link() ->
 
 init([]) ->
     [M:sup_init() || M <- [otter_snapshot_count, otter_conn_zipkin]],
-    {ok, { {one_for_all, 0, 1}, []} }.
+    ChildSpecs = case otter_config:read(server_zipkin_callback) of
+        {ok, {_CbMod, _CbFun}} ->
+            [cowboy_spec()];
+        _ ->
+            []
+    end,
+    {ok, { {one_for_all, 0, 1}, ChildSpecs} }.
+
+
+cowboy_spec() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/[...]", otter_server_zipkin_handler, []}
+        ]}
+    ]),
+    ListenPort = otter_config:read(server_zipkin_port, 9411),
+    ranch:child_spec(
+        otter_server_zipkin,
+        100,
+        ranch_tcp,
+        [
+            {port, ListenPort},
+            {max_connections, 100},
+            {backlog, 1024}
+        ],
+        cowboy_protocol,
+        [{env, [{dispatch, Dispatch}]}]
+    ).
