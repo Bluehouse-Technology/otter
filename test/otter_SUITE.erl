1be0b6375ce2d10beabbe680629eb58a0cfa6e46
Chandrashekhar Mullaparthi <chandrashekhar.mullaparthi@gmail.com>
2017-04-19 03:20:37 +0100

    Make cowboy an optional dependency

--- test/otter_SUITE.erl
+++ test/otter_SUITE.erl
@@ -7,6 +7,7 @@ all() ->
     [ftest, ptest].
 
 ptest(_Config) ->
+    application:ensure_all_started(cowboy),
     application:ensure_all_started(otter),
     application:set_env(otter, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
     application:set_env(otter, server_zipkin_callback, {?MODULE, handle_span}),
@@ -20,6 +21,7 @@ ptest(_Config) ->
     [_] = ets:tab2list(test_span_collector).
 
 ftest(_Config) ->
+    application:ensure_all_started(cowboy),
     application:ensure_all_started(otter),
     application:set_env(otter, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
     application:set_env(otter, server_zipkin_callback, {?MODULE, handle_span}),
